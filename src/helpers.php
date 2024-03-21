21d4d5402167934446bda66880015f52651794d4
OniiCoder <oniicoder@gmail.com>
2024-03-21 06:59:21 +0000

    Use pretty print

--- src/helpers.php
+++ src/helpers.php
@@ -4,8 +4,6 @@ namespace OniiCoder\OniDebugger;
 
 if (! function_exists('oni')) {
     function oni(...$args) {
-        file_put_contents(storage_path('logs/debug.log'), json_encode($args) . PHP_EOL, FILE_APPEND);
-        // You can customize the behavior of the debug function here
-        // For example, you can log to different files, or use different formatting
+        file_put_contents(storage_path('logs/debug.log'), json_encode($args, JSON_PRETTY_PRINT) . PHP_EOL, FILE_APPEND);
     }
 }
