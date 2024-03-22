f83cadf63090fce91f06f1a5c08932e0dba083b8
OniiCode <oniicoder@gmail.com>
2024-03-22 21:42:03 +0000

    Write caller file and line to file

--- src/helpers.php
+++ src/helpers.php
@@ -4,6 +4,25 @@ namespace OniiCoder\OniDebugger;
 
 if (! function_exists('oni')) {
     function oni(...$args) {
-        file_put_contents(storage_path('logs/debug.log'), json_encode($args, JSON_PRETTY_PRINT) . PHP_EOL, FILE_APPEND);
+        $caller = getCallerClassAndMethod();
+
+        writeToFile($caller);
+
+        foreach ($args as $arg) {
+            writeToFile($arg);
+        }
     }
+}
+
+function writeToFile($data) {
+    file_put_contents(storage_path('logs/debug.log'), json_encode($data, JSON_PRETTY_PRINT) . PHP_EOL, FILE_APPEND);
+}
+
+function getCallerClassAndMethod() {
+    $trace = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 2);
+    if (isset($trace[1]['file']) && isset($trace[1]['line'])) {
+        return $trace[1]['file'] . ':' . $trace[1]['line'];
+    }
+
+    return null;
 }
